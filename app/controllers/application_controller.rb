547b03d89a13977ccba7f1c45017d84682dc1119
tamam03 <tamam0308@gmail.com>
2022-04-08 08:11:49 +0000

    fontawesome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-    about_path
+    user_path(current_user)
   end
 
   def after_sign_out_path_for(resource)
